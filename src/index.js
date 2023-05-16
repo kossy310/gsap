dd861bc0892910af0c8ff4c981b49c72bee615e9
kossy310 <nakakoshi3100@gmail.com>
2023-05-17 03:01:24 +0900

    一文字づつのgsap

--- src/index.js
+++ src/index.js
@@ -1,56 +1,39 @@
 import {gsap} from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 //変数定義
-const jsLoaderBg = '.js-loader-bg'; // カーテン（黒い背景）
-
-//キャッチコピーを一文字づつに
-  let animationArea = document.querySelector(".animation-title")
-  const animationTextArray =animationArea.innerHTML.split('');
-  console.log(animationTextArray);
-  let animationWord = "";
-  for (let i = 0; i < animationTextArray.length; i++) {
-    animationWord += '<span>' + animationTextArray[i] + '</span>';
-  };
-  animationArea.innerHTML = animationWord;
+const target = document.querySelectorAll('.title'); 
+target.forEach(item => {
+  console.log(item)
+  gsap.from(
+    item.children,{
+      y:50,
+      opacity:0,
+      rotate:60,
+      stagger:{
+        each:0.3,
+      },
+      scrollTrigger: {
+        trigger: item,
+        start: 'top 70%',
+        markers:true,
+      },
+      
+    })
+})
 
 
 //gsap
-gsap.set(".image",{
-  width:"0px",
-  opacity:0,
-})
 
-const tl = gsap.timeline();
+// const tl = gsap.timeline();
 
-tl.set(
-  ".title span",{
-    opacity:0,
-  })
-  .to(
-    ".c-loader-bg", {
-      y: '100%'
-    },
-    '+=0.5'
-).set(
-".title span",{
-  opacity:0,
-}).from(
-  ".box",{
-    opacity:1,
-    x:'-100%',
-  },'+=0.3'
-).to(
-  ".box",{
-    opacity:1,
-    x:'100%',
-  },'+=0.6'
-).to(
-  ".title span",{
-    opacity:1,
-  },'-=0.3').to(".image",{
-    width:"400px",
-    opacity:1,
-  }).from(".animation-title span",{
-    autoAlpha:0,y:20,ease: "power4.out",stagger:0.05
-  },'+=1')
-
+// tl.to(
+//   target,{
+//     y:50,
+//     stagger:{
+//       each:0.5,
+//       ease:"Power4.inOut",
+//     }
+//   })
+ 
